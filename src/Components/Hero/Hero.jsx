5c918b67ce00fe994fa142971144dc902d84dab3
Ravish <ravish.rajput@vnaitservices.com>
2024-09-20 19:30:03 +0530

    Updated components and removed unused assets

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,17 +1,35 @@
-import React from 'react'
-import './Hero.css'
-import dark_arrow from '../../assets/dark-arrow.png'
+import React from 'react';
+import './Hero.css';
+import dark_arrow from '../../assets/dark-arrow.png'; // Remove this import if not needed
+import facebook_1 from '../../assets/Facebook.png';
+import instagram_2 from '../../assets/Instagram.png';
+import youtube_3 from '../../assets/Youtube.png';
+import gmail_4 from '../../assets/Gmail.png';
 
 const Hero = () => {
   return (
     <div className='hero container'>
       <div className="hero-text">
-        <h1>FREEE RECYCLE INDIA</h1>
+        <h1>BHARAT RECYCLE INDIA</h1>
         <p>BharatRecycles Limited is a Waste Recycling and Rubber Manufacturing Company aimed at solving the problem of waste tyres by converting them into reusable rubber moulded products</p>
-        <button className='btn'>Explore more <img src={dark_arrow} alt="" /></button>
+        
+        <div className='social-media'>
+          <a href='https://facebook.com' target='_blank' rel='noopener noreferrer'>
+            <img src={facebook_1} alt="Facebook" />
+          </a>
+          <a href='https://instagram.com' target='_blank' rel='noopener noreferrer'>
+            <img src={instagram_2} alt="Instagram" />
+          </a>
+          <a href='https://youtube.com' target='_blank' rel='noopener noreferrer'>
+            <img src={youtube_3} alt="YouTube" />
+          </a>
+          <a href='mailto:example@gmail.com' target='_blank' rel='noopener noreferrer'>
+            <img src={gmail_4} alt="Gmail" />
+          </a>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
